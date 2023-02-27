7d587e35d763ed1ba3ca3ee8a8a3b068eb0725fa
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-02-27 13:39:20 -0800

    Initialize starter React files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>People of 301</h1>
+        </header>
+        <main>
+          <article>
+            <h3>Sheyna</h3>
+            <p>text here</p>
+          </article>
+        </main>
+        <footer>
+        </footer>
+      </>
+    ); 
+  }
+};
 
 export default App;
