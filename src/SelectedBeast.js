90e44eed7f436b00ac70514d85077e56eb5fd674
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-03-01 11:11:43 -0800

    Add SelectedBeast component with boilerplate

---
+++ src/SelectedBeast.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SelectedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+       return; 
+    }
+};
+
+export default SelectedBeast;
